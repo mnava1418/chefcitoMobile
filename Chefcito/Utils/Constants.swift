a909f84b40d18c9e2ff71df640a773a8cda1d4ee
mnava1418 <mnavapena@gmail.com>
2020-11-20 18:18:05 -0600

    remove tokens

--- Chefcito/Utils/Constants.swift
+++ Chefcito/Utils/Constants.swift
@@ -11,5 +11,4 @@ struct Constants {
     public static let GENERIC_ERROR = "Se ha producido un error. Intenta mas tarde."
     public static let HOST = "http://localhost:3000"
     public static let TOKEN_KEY = "token"
-    public static let SM_TOKEN = "SocialMedia1418"
 }
