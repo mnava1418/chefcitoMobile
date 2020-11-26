09a4bb23d4c036568bbefc7140006272a91c70a3
mnava1418 <mnavapena@gmail.com>
2020-11-25 19:27:36 -0600

    set view to add recipe

---
+++ Chefcito/Views/ChefcitoItemsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ChefcitoItemsViewController.swift
+//  Chefcito
+//
+//  Created by Martin Nava Pe&a on 25/11/20.
+//
+
+import UIKit
+
+class ChefcitoItemsViewController: UIViewController {
+
+    public var navTitle:ItemsTile!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setNavBar()
+    }
+    
+    private func setNavBar() {
+        self.title = navTitle.rawValue
+        let plusCircleImg = UIImage(systemName: "plus")
+        let addButton = UIBarButtonItem(image: plusCircleImg, style: .plain, target: self, action: #selector(addItem))
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    @objc private func addItem() {
+        if navTitle == ItemsTile.recetas {
+            performSegue(withIdentifier: "addReceipt", sender: nil)
+        }
+    }
+       
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Chefcito/Views/ChefcitoViewController.swift
+++ Chefcito/Views/ChefcitoViewController.swift
@@ -7,10 +7,17 @@
 
 import UIKit
 
+enum ItemsTile:String {
+    case recetas = "Recetas"
+    case menus = "Menus"
+}
+
 class ChefcitoViewController: UIViewController {
 
     @IBOutlet weak var btnReceips: UIButton!
     @IBOutlet weak var btnMenus: UIButton!
+    
+    private var itemsTitle:ItemsTile!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,20 +31,28 @@ class ChefcitoViewController: UIViewController {
         btnMenus.setMainButton()
     }
     
+    @IBAction func showReceips(_ sender: Any) {
+        itemsTitle = .recetas
+        self.performSegue(withIdentifier: "showItems", sender: nil)
+    }
+    
+    @IBAction func showMenus(_ sender: Any) {
+        itemsTitle = .menus
+        self.performSegue(withIdentifier: "showItems", sender: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let destination = segue.destination as! ChefcitoItemsViewController
+        destination.navTitle = itemsTitle
     }
-    */
-
 }
 
 extension UIButton {
