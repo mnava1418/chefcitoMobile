8859054484cd07a8b3d62355261c475029118c7f
mnava1418 <mnavapena@gmail.com>
2020-11-30 18:24:02 -0600

    set main view for new recipe

--- Chefcito/Views/NewRecipeViewController.swift
+++ Chefcito/Views/NewRecipeViewController.swift
@@ -9,10 +9,14 @@ import UIKit
 
 class NewRecipeViewController: UIViewController {
 
-    @IBOutlet weak var inputName: UITextField!
-    @IBOutlet weak var pickerCategory: UIPickerView!
-    @IBOutlet weak var ingredientTable: UITableView!
-    @IBOutlet weak var txtInstructions: UITextView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var btnOne: UIButton!
+    @IBOutlet weak var btnTwo: UIButton!
+    @IBOutlet weak var btnThree: UIButton!
+    
+    private var primaryColor: UIColor!
+    private var secondaryColor: UIColor!
     
     private let categories = ["Entrada", "Sopa", "Plato fuerte", "Postre"]
     private var ingredients:[String] = []
@@ -21,13 +25,37 @@ class NewRecipeViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        pickerCategory.dataSource = self
-        pickerCategory.delegate = self
-        ingredientTable.dataSource = self
-        ingredientTable.delegate = self
+        scrollView.delegate = self
+        btnOne.tag = 1
+        btnTwo.tag = 2
+        btnThree.tag = 3
+        
+        primaryColor = btnOne.tintColor
+        secondaryColor = btnTwo.tintColor
     }
     
-    private func showNewIngredientForm() {
+    private func scrollToPage (page: Int) {
+         var frame: CGRect = self.scrollView.frame
+        frame.origin.x = frame.size.width * CGFloat(page)
+        frame.origin.y = 0
+        self.scrollView.scrollRectToVisible(frame, animated: true)
+    }
+    
+    private func setIndicators (currentPage: Int) {
+        pageControl.currentPage = currentPage - 1
+        btnOne.setStatusColor(currentPage: currentPage, primaryColor: primaryColor, secondaryColor: secondaryColor)
+        btnTwo.setStatusColor(currentPage: currentPage, primaryColor: primaryColor, secondaryColor: secondaryColor)
+        btnThree.setStatusColor(currentPage: currentPage, primaryColor: primaryColor, secondaryColor: secondaryColor)
+    }
+    
+    @IBAction func scrollToPageBtn(_ sender: Any) {
+        let btn = sender as! UIButton
+        scrollToPage(page: btn.tag - 1)
+        setIndicators(currentPage: btn.tag)
+    }
+    
+    
+    /*private func showNewIngredientForm() {
         let addIngredientScreen = UIAlertController(title: "Ingrediente", message: "", preferredStyle: .alert)
         addIngredientScreen.addTextField { textField in
             textField.placeholder = "Ingrediente"
@@ -46,11 +74,8 @@ class NewRecipeViewController: UIViewController {
         addIngredientScreen.addAction(okAction)
         
         self.present(addIngredientScreen, animated: true)
-    }
+    }*/
     
-    @IBAction func addIngredient(_ sender: Any) {
-        showNewIngredientForm()
-    }
     /*
     // MARK: - Navigation
 
@@ -63,8 +88,30 @@ class NewRecipeViewController: UIViewController {
 
 }
 
+extension UIButton {
+    func setStatusColor(currentPage: Int, primaryColor: UIColor, secondaryColor: UIColor) {
+        if self.tag == currentPage {
+            self.tintColor = primaryColor
+        } else {
+            self.tintColor = secondaryColor
+        }
+    }
+}
+
+extension UIScrollView {
+    var currentPage:Int{
+        return Int((self.contentOffset.x+(0.5*self.frame.size.width))/self.frame.width)+1
+    }
+}
+
+extension NewRecipeViewController: UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        setIndicators(currentPage: scrollView.currentPage)
+    }
+}
+
 // MARK: - Picker View
-extension NewRecipeViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+/*extension NewRecipeViewController: UIPickerViewDataSource, UIPickerViewDelegate {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -77,10 +124,10 @@ extension NewRecipeViewController: UIPickerViewDataSource, UIPickerViewDelegate 
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return categories[row]
     }
-}
+}*/
 
 // MARK: - Table View
-extension NewRecipeViewController: UITableViewDataSource, UITableViewDelegate {
+/*extension NewRecipeViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return ingredients.count
     }
@@ -104,4 +151,4 @@ extension NewRecipeViewController: UITableViewDataSource, UITableViewDelegate {
         actions.performsFirstActionWithFullSwipe = true
         return actions
     }
-}
+}*/
